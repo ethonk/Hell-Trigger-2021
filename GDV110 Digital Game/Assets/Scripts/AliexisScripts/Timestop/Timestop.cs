9209e9cd5308ab0ac4a97c3466d258a65597bd72
HaremTime <aliexisvg.alvarez@gmail.com>
2021-11-05 18:12:02 +1300

    Pain
    Hjelp

--- GDV110 Digital Game/Assets/Scripts/AliexisScripts/Timestop/Timestop.cs
+++ GDV110 Digital Game/Assets/Scripts/AliexisScripts/Timestop/Timestop.cs
@@ -3,21 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Timestop : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{   
+    public int Stopduration;
+    public GameObject cam;
+    public void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        cam = GameObject.Find("Main Camera");
     }
     
     public void ApplyTimestop(GameObject _collideObj)
     {
+        MonoBehaviour CamMono = cam.GetComponent<MonoBehaviour>();
         print ("freeze nigga, the niggas name is: " + _collideObj.name);
+        Debug.Log("Active? "+gameObject.activeInHierarchy);
+        CamMono.StartCoroutine(TimeStopCoroutine(_collideObj));
+    }
+
+    IEnumerator TimeStopCoroutine(GameObject _StoppedObj)
+    {   
+        _StoppedObj.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+        yield return new WaitForSeconds(Stopduration);
+        _StoppedObj.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
     }
 }
