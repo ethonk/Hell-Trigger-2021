aa1f5dad96804810804586ea71a66055f296e635
HaremTime <aliexisvg.alvarez@gmail.com>
2021-11-16 20:27:12 +1300

    StartingElevator
    ElevatorStarted

---
+++ GDV110 Digital Game/Assets/Scripts/AliexisScripts/Elevator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    //starting position - automatically set at the start
+    public float StartPos;
+    //ending position - set this for end destination (how high)
+    public float EndPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = gameObject.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
