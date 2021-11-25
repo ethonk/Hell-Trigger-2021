ed5feedee8280e8d5a45fbb19db490bb3207e792
ethonk <62923028+ethonk@users.noreply.github.com>
2021-11-25 18:33:01 +1300

    b reh

---
+++ GDV110 Digital Game/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalScript : MonoBehaviour
+{
+    public string SceneName;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.root.gameObject.layer == 8)
+        {
+            SceneManager.LoadScene(SceneName, LoadSceneMode.Single);
+        }
+    }
+}
